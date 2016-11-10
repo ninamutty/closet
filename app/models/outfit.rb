eda7dca42e2e6b05a23bc7652ebcbbf7510226ea
Nina Mutty <ninamutty@gmail.com>
2016-11-09 22:06:04 -0800

    added foundation

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -3,7 +3,7 @@ class Outfit < ActiveRecord::Base
   has_many :tags, :through => :outfit_tags
   belongs_to :user
 
-  has_attached_file :photo, :styles => {:small => '200x100>', :medium => "300x200>"}
+  has_attached_file :photo, :styles => {:small => '200x100>', :medium => "450x300>", :large => "700x500"}
 
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
