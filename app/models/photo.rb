cf1c6d85aa233052b72a4f3af5110f309b4af6b2
Nina Mutty <ninamutty@gmail.com>
2016-11-30 14:17:50 -0800

    aws credentials

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,11 @@ class Photo < ActiveRecord::Base
   belongs_to :outfit
 
 
-  has_attached_file :image, :styles => {:small => '200x100>', :medium => "450x300>", :large => "630x420"}
+  has_attached_file :image, :styles => {:small => '200x100>', :medium => "450x300>", :large => "630x420"},
+    storage: :s3,
+    s3_credentials: {access_key_id: ENV["AWS_KEY"], secret_access_key: ENV["AWS_SECRET"]},
+    bucket: ENV["AWS_BUCKET"]
+
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
