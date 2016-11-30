a72cedce78dd0fead6ea629ced10a05c2a78fb1e
Nina Mutty <ninamutty@gmail.com>
2016-11-30 14:27:53 -0800

    add paperclip url

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,7 +5,8 @@ class Photo < ActiveRecord::Base
   has_attached_file :image, :styles => {:small => '200x100>', :medium => "450x300>", :large => "630x420"},
     storage: :s3,
     s3_credentials: {access_key_id: ENV["AWS_KEY"], secret_access_key: ENV["AWS_SECRET"]},
-    bucket: ENV["AWS_BUCKET"]
+    bucket: ENV["AWS_BUCKET"],
+    url: "s3.amazonaws.com/#{ENV["AWS_BUCKET"]}"
 
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
